26c95263d62b568aa6dc36d641e027f4517161b6
dinhinfotech <dinhinfotech@gmail.com>
2019-05-07 17:05:46 +0200

    Update load_data.py

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -6,6 +6,10 @@ from lib import gp2lib
 data_folder = "test_data"
 
 # Use_sf and use_entr are parameters corresponding to site features, be careful when using use_entr since they are biased
+# use_up: contains 1 feature
+# use_con: 2 features
+# use_str_elem_up: 4 features
+# bpp: 1 feature
 graphs, seqs_1h, sfv_list, labels = gp2lib.load_data(data_folder,
                                            use_up=True,
                                            use_con=True,
