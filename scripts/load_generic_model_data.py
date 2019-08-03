1429a1c070337e24620e8ef35e13a02f437facef
Michael Uhl <uhl.michael@gmx.de>
2019-08-03 17:37:41 +0200

    edit generic load

--- scripts/load_generic_model_data.py
+++ scripts/load_generic_model_data.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 from lib import gp2lib
+import re
 
 # Input data folder to load data from.
-data_folder = "test10_generic_set_extlr30_extcon150_thr2_m0_out"
+data_folder = "/home/uhlm/scratch_0_uhlm/Data/cluster/gp2_out_merged_results"
+# /home/uhlm/scratch_0_uhlm/Data/cluster/gp2_out_merged_results
+# test10_generic_set_extlr30_extcon150_thr2_m0_out
 
 graphs, seqs_1h, sfv_list, labels = gp2lib.load_data(data_folder,
                                            use_up=True,
@@ -12,12 +15,12 @@ graphs, seqs_1h, sfv_list, labels = gp2lib.load_data(data_folder,
                                            use_entr=False,
                                            gm_data=True,
                                            onehot2d=False,
-                                           mean_norm=True,
                                            add_1h_to_g=True,
+                                           vp_ext = 10,
                                            use_str_elem_up=True,
                                            use_str_elem_1h=False,
                                            use_us_ds_labels=False,
-                                           bpp_cutoff=0.2)
+                                           bpp_cutoff=1)
 
 
 # Get RBP label to numeric label ID (stored in labels list) mapping.
@@ -25,7 +28,7 @@ label_dic = {}
 for i,g in enumerate(graphs):
     m = re.search("(.+?)_", g.graph["id"])
     label_dic[m.group(1)] = labels[i]
-print ("RBP label to label ID mapping:")
+print("RBP label to label ID mapping:")
 for label_id, label_idx in sorted(label_dic.items()):
     print("%s -> %i" %(label_id, label_idx))
 
