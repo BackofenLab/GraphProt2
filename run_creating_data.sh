61c5ab358c1e0847c6396d16c461068d114c96bf
dinhinfotech <dinhinfotech@gmail.com>
2019-09-09 15:55:44 +0200

    Update run_creating_data.sh

--- run_creating_data.sh
+++ run_creating_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mode=2 # 0: direct, 1: indirect (loading graphs), 2: generic
+mode=2 # 0: direct, 1: indirect (loading graphs), indirect_hops, 3: generic
 
 if [ ${mode} == 0 ]; then
   python create_geometric_direct.py \
@@ -13,6 +13,12 @@ elif [ ${mode} == 1  ]; then
     --out_dataset AGGF1 \
     --in_folder /home/dinh/data/projects/code/pycharm/graphprot2/data/raw_cv \
     --out_folder /home/dinh/data/projects/code/pycharm/graphprot2/data/geometric_cv
+elif [ ${mode} == 2  ]; then
+  python create_geometric_indirect_hops.py \
+    --in_dataset AGGF1 \
+    --out_dataset AGGF1 \
+    --in_folder /home/dinh/data/projects/code/pycharm/graphprot2/data/raw_cv \
+    --out_folder /home/dinh/data/projects/code/pycharm/graphprot2/data/geometric_cv    
 else
     python create_geometric_generic.py \
     --in_dataset GENERIC \
